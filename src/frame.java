4df7d56f8bcd2be1eb22f6545e81608c2dc88706
Dean Moore <deanmoore009@gmail.com>
2018-02-10 18:06:24 +0000

    Initial commit

---
+++ src/frame.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class frame extends JFrame{
+	
+	
+	
+}
---
+++ src/menu.java
@@ -0,0 +1,25 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+
+public class menu extends JFrame implements ActionListener{
+	
+	public menu () {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
